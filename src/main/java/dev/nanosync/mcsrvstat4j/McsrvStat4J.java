6d04bd8627d10b9fa886aed56dc51b27715d90c8
Martin Hartfelder <martinhartfelder@mx2.unisc.br>
2022-01-21 22:02:22 -0300

    👾 Added Exception

--- src/main/java/dev/nanosync/mcsrvstat4j/McsrvStat4J.java
+++ src/main/java/dev/nanosync/mcsrvstat4j/McsrvStat4J.java
@@ -2,8 +2,10 @@ package dev.nanosync.mcsrvstat4j;
 
 import dev.nanosync.mcsrvstat4j.service.ServerService;
 
+import javax.management.AttributeNotFoundException;
+
 public class McsrvStat4J {
-    public ServerService getStatus(String host){
+    public ServerService getStatus(String host) throws AttributeNotFoundException {
         return new ServerService(host);
     }
 }
